ec7aad92be163953aa405ee3abb31d8f8f336a44
Michał Lach <kontakt@michal-lach.pl>
2018-11-08 13:32:38 +0100

    stuff

--- video-browser/src/App.vue
+++ video-browser/src/App.vue
@@ -1,32 +1,11 @@
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
-
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    hello
   </div>
 </template>
 
-
-
-
+<script>
+  export default {
+    
+  }
+</script>
